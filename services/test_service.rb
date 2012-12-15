fd3c9f3b7eea006ba0c9a022847ae3aacb0f6e25
Marek Goldmann <marek.goldmann@gmail.com>
2012-12-15 11:59:42 +0100

    Added topics

--- services/test_service.rb
+++ services/test_service.rb
@@ -4,11 +4,16 @@ class TestService
   def initialize(options = {})
     @queue = TorqueBox::Messaging::Queue.new('/queues/test')
     @queue1 = fetch('/queues/test')
+    @topic = TorqueBox::Messaging::Topic.new('/topics/test')
+    @topic1 = fetch('/topics/test')
   end
 
   def start
     puts @queue.class
     puts @queue1.class
+
+    puts @topic.class
+    puts @topic1.class
 
     @queue.publish "test"
     @queue1.publish "test"
