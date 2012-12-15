83565ce530a949037110f4aa2a5b818235ad8ebd
Marek Goldmann <marek.goldmann@gmail.com>
2012-12-15 11:42:09 +0100

    Change the order, publish a test message

--- services/test_service.rb
+++ services/test_service.rb
@@ -2,13 +2,16 @@ class TestService
   include TorqueBox::Injectors
 
   def initialize(options = {})
-    @queue = fetch('/queues/test')
-    @queue1 = TorqueBox::Messaging::Queue.new('/queues/test')
+    @queue = TorqueBox::Messaging::Queue.new('/queues/test')
+    @queue1 = fetch('/queues/test')
   end
 
   def start
     puts @queue.class
     puts @queue1.class
+
+    @queue.publish "test"
+    @queue1.publish "test"
   end
 
   def stop
